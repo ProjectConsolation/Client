6041b5dcf3ae50854ad16ce1cf0dd47105435662
ujicos <94710096+ujicos@users.noreply.github.com>
2024-03-07 22:28:17 +0100

    feat: crippled player

--- src/component/patches.cpp
+++ src/component/patches.cpp
@@ -170,14 +170,14 @@ namespace patches
 			utils::hook::nop(game::game_offset(0x102489A1), 5);
 #endif
 
-			dvars::overrides::register_int("g_speed", 210, 0, 1000, game::dvar_flags::saved);
+			//dvars::overrides::register_int("g_speed", 210, 0, 1000, game::dvar_flags::saved);
 
 			dvars::overrides::register_float("r_lodScale", 0.0, 0, 3, game::dvar_flags::saved); //Doesnt want to be set
 			dvars::overrides::register_float("ui_smallFont", 0.0, 0, 1, game::dvar_flags::saved);
 			dvars::overrides::register_float("ui_bigFont", 0.0, 0, 1, game::dvar_flags::saved);
 			dvars::overrides::register_float("ui_extraBigFont", 0.0, 0, 1, game::dvar_flags::saved);
 			dvars::overrides::register_float("cg_overheadNamesSize", 0.5, 0, 1, game::dvar_flags::saved);
-			dvars::overrides::register_float("jump_height", 39.0, 0, 99999, game::dvar_flags::saved);
+			//dvars::overrides::register_float("jump_height", 39.0, 0, 99999, game::dvar_flags::saved); //doesnt work for me but works for liam
 			dvar_registernew_hook.create(game::Dvar_RegisterNew, Dvar_RegisterNew_Stub);
 		}
 	};
